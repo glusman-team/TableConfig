b739707623721cd0efc07f829ece54b66e84392c
SkyeAv <skye.a.goetz@gmail.com>
2025-10-12 15:29:01 -0700

    Skye Lane Goetz (made fastmerge importable) Sun Oct 12 03:29:01 PM PDT 2025

--- src/TableConfig/yaml.py
+++ src/TableConfig/yaml.py
@@ -6,14 +6,14 @@ from pathlib import Path
 from typing import Any
 import yaml
 
-def _fastmerge(a: Any, b: Any) -> Any:
+def fastmerge(a: Any, b: Any) -> Any:
   # streamlined (fast) implementation of deepmerge config
   if isinstance(a, dict) and isinstance(b, dict):
     for k, v in b.items():
       if k in a:
         av: Any = a[k]
         if isinstance(av, dict) and isinstance(v, dict):
-          _fastmerge(av, v)
+          fastmerge(av, v)
         elif isinstance(av, list) and isinstance(v, list):
           av.extend(v)
         else:
@@ -36,4 +36,4 @@ def load(yaml_p: Path) -> list[dict[str, Any]]:
     raw: Any = yaml.load(f, Loader=Loader)
     tc: TableConfig = TableConfig.model_validate(raw)
     template: dict[str, Any] = tc.template
-    return [_fastmerge(template, section) for section in tc.section]
+    return [fastmerge(template, section) for section in tc.section]
